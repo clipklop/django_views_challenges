2532fec0c4da09af6c2591d6de2151b31f36193e
Mischa <mishayartsev@gmail.com>
2023-09-26 23:29:04 +0300

    +a and b challenges

--- django_views_routing_homework/views/level_3/b_validate_user_data.py
+++ django_views_routing_homework/views/level_3/b_validate_user_data.py
@@ -17,9 +17,57 @@
 Для тестирования рекомендую использовать Postman.
 Когда будете писать код, не забывайте о читаемости, поддерживаемости и модульности.
 """
+import json
 
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, JsonResponse, HttpResponseNotAllowed
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+
+
+def is_full_name_valid(data: dict[str, str]) -> bool:
+    if data.get('full_name'):
+        return len(data['full_name']) >= 5 and len(data['full_name']) <= 256
+    return None
+
+
+def is_email_valid(data: dict[str, str]) -> bool:
+    if data.get('email'):
+        try:
+            validate_email(data['email'])
+            return True
+        except ValidationError:
+            return False
+
+
+def is_registered_from_valid(data: dict[str, str]) -> bool:
+    if data.get('registered_from'):
+        return data['registered_from'] in ["website", "mobile_app"]
+    return None
+
+
+def is_age_valid(data: dict[str, str]) -> bool:
+    if data.get('age'):
+        return isinstance(data['age'], int)
+    return None
 
 
 def validate_user_data_view(request: HttpRequest) -> HttpResponse:
-    pass  # код писать тут
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            if data.get('age'):
+                if is_full_name_valid(data) and is_email_valid(data) \
+                    and is_registered_from_valid(data) and is_age_valid(data):
+                    return JsonResponse(data={"is_valid": True}, safe=False, status=200)
+                
+                return JsonResponse(data={"is_valid": False}, safe=False, status=200)
+
+            if is_full_name_valid(data) and is_email_valid(data) \
+                and is_registered_from_valid(data):
+                return JsonResponse(data={"is_valid": True}, safe=False, status=200)
+            
+            return JsonResponse(data={"is_valid": False}, safe=False, status=200)
+        except Exception:
+            return JsonResponse(data={}, status=400)
+    
+    return HttpResponseNotAllowed(permitted_methods=['POST'])
